6fb0c2f183b33c90b4f16bdf3edc1b17dde6ab1a
TinMartinn <95651717+TinMartinn@users.noreply.github.com>
2022-08-01 23:27:23 -0300

    Add files via upload

--- MartinArzamendia/scripts/scripts.js
+++ MartinArzamendia/scripts/scripts.js
@@ -1,28 +1,33 @@
-function bienvenidx() {
-    alert("HOLA! te vamos a hacer unas preguntas y si no las sabes sos re tonto");
-}
 function sigPregunta() {
     alert("no sos tan tonto como pensaba, siguiente pregunta");
 }
-bienvenidx();
 
-let debut = prompt("¿En que año debuto NUEST?")
+class persona {
+    constructor(nombre) {
+        this.nombre = nombre;
+    }
+}
+
+const persona1 = new persona(nombre = prompt("primero decime como te llamas no?"));
+
+alert(`${this.nombre} te vamos a hacer unas preguntas y si no las sabes es porque sos re tonto`);
+
+
+let debut = parseInt(prompt("¿En que año debuto NUEST?"));
 while (debut != 2012) {
     alert("RESPUESTA INCORRECTA bobi");
-    debut = prompt("intentelo de nuevo");
+    debut = prompt("intentelo de nuevo ");
 }
 
 sigPregunta();
 
-let pregunta2 = prompt("¿Cuanto mide un perro promedio?")
+let pregunta2 = prompt("¿Cuanto mide un perro promedio?");
 if (pregunta2 >= 15 && pregunta2 <= 110) {
     alert("como vas a saber eso? no tenes vida? ajjajaja")
 } else {
     alert("alto tontito como no sabes? jajaja")
     pregunta2 = prompt("Cuanto mide un perro promedio?")
 }
-
-sigPregunta()
 
 alert("la verdad me cansaste y auque te siga haciendo mas preguntas ya se que sos re bobito")
 
